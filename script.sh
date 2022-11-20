0d989daf39438c33ad719b5dc67e55cb3907971b
Jakub Zając <93821978+Jcobzajac@users.noreply.github.com>
2022-11-20 21:12:36 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#https://stackoverflow.com/questions/37290935/replace-value-of-a-line-in-a-yml-with-bash
-
-#\/ == /
-
-# . == any character container_image
-
 cd application
 
 sed -i -r "s/^(\s*)(container_image\s*:\s*644435390668.dkr.*\s*$)/\1container_image: 644435390668.dkr.ecr.ap-northeast-3.amazonaws.com\/appcertification:$1/" values.yaml
